4b9b1bea8ed2c0310f299e6032d94d4e9dfc4832
canrager <canrager@gmail.com>
2025-01-05 12:09:00 -0500

    clean up

--- utils/project_config.py
+++ utils/project_config.py
@@ -1,6 +1,13 @@
+import os
 from pathlib import Path
 
 ROOT_DIR = Path(__file__).parent.parent
 DATA_DIR = ROOT_DIR / "data"
 INTERIM_DIR = ROOT_DIR / "artifacts" / "interim"
 PLOT_DIR = ROOT_DIR / "artifacts" / "plot"
+
+# Make directories if they don't exist
+dirs = [DATA_DIR, INTERIM_DIR, PLOT_DIR]
+for d in dirs:
+    if not os.path.exists(d):
+        os.makedirs(d)
